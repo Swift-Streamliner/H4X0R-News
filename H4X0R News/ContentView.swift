cb48efce0525f3f132645159f0e330a1e185f9d6
thisisstreamliner <thisisstreamliner@gmail.com>
2020-11-02 10:40:29 +0200

    Adding List and Model class

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -10,7 +10,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+    
+        
     }
 }
 
@@ -19,3 +26,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Post : Identifiable {
+    let id : String
+    let title : String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
