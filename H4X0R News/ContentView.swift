41e32007059222db6231bb0071351d76f08bd5c7
thisisstreamliner <thisisstreamliner@gmail.com>
2020-11-02 10:13:06 +0200

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Tiger Mei on 02.11.2020.
+//  Copyright © 2020 Tiger Mei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
